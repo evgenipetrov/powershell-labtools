44a812a5247db98397d2c7ee3318c1589948ea6f
evgeni <evgeni.petrov@gmail.com>
2017-05-17 04:22:18 -0700

    Added 2 commandlets

---
+++ PowerShell.LabTools/Enable-LTRunEverythingAsAdministrator.ps1
@@ -0,0 +1,19 @@
+﻿function Enable-LTRunEverythingAsAdministrator
+{
+  <#
+    .SYNOPSIS
+    Short Description
+    .DESCRIPTION
+    Detailed Description
+    .EXAMPLE
+    Enable-LTRunEverythingAsAdministrator
+    explains how to use the command
+    can be multiple lines
+    .EXAMPLE
+    Enable-LTRunEverythingAsAdministrator
+    another example
+    can have as many examples as you like
+  #>
+  # run everything as administrator
+  Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value "0"
+}
---
+++ PowerShell.LabTools/Join-LTDomainComputer.ps1
@@ -0,0 +1,30 @@
+﻿function Join-LTDomainComputer
+{
+  <#
+      .SYNOPSIS
+      Short Description
+      .DESCRIPTION
+      Detailed Description
+      .EXAMPLE
+      Join-LTDomainComputer
+      explains how to use the command
+      can be multiple lines
+      .EXAMPLE
+      Join-LTDomainComputer
+      another example
+      can have as many examples as you like
+  #>
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory=$true, Position=1)]
+    [String]$DomainName,
+ 
+    [Parameter(Mandatory=$true, Position=2)]
+    [System.Management.Automation.PSCredential]
+    $Credential
+  )
+ 
+  
+  Add-Computer -DomainName $DomainName -Credential $Credential -Restart
+}
